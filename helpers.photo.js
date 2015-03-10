6e0c60bced5bec983d7e366dac11a4fb3deac26f
Gauthier Deroo <g.deroo@gmail.com>
2015-03-10 10:58:05 +0100

    add new circleCI command

--- helpers.photo.js
+++ helpers.photo.js
@@ -1,10 +1,8 @@
 if (Meteor.isClient) {
     Template.photos.helpers({
         photos: function() {
-            return Photos.find({}, {sort: {'createdAt': -1}});
+            return Photos.find({}, {sort: {'createdAt': -1}})
         }
     });
-
-
 
 }
